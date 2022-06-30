ef7ac0394bcebcd97b235d6994183a25c57f7d40
Jigish Solanki <jigish.solanki@intesols.com.au>
2022-06-30 15:16:26 +0530

    Remove unwanted code

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,5 @@
 import React from "react";
 
-class SeachBar1 extends React.Component {
-  state = { term: "" };
-
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    this.props.onSubmit(this.state.term);
-  };
-
-  render() {
-    return (
-      <div className="ui segment">
-        {/* <form className="ui form" onSubmit={this.onFormSubmit}> */}
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-
 class SeachBar extends React.Component {
   state = { term: "" };
 
