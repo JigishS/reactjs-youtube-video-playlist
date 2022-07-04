f46b8b792f3b1cfa667122cf7218afc20ce87a83
Jigish Solanki <jigish.solanki@intesols.com.au>
2022-07-04 07:34:49 +0530

    Remove unused code

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,5 @@
 import React, { useState } from "react";
 
-class SearchBar1 extends React.Component {
-  state = { term: "" };
-
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = (event) => {
-    event.preventDefault();
-
-    this.props.onFormSubmit(this.state.term);
-  };
-
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
-
 const SearchBar = ({ onFormSubmit }) => {
   const [term, setTerm] = useState("");
 
